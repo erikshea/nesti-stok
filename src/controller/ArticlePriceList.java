7a93409a5819809f67e4e06ee15a48dcfe44e048
Manu <emmanuellegay.13@gmail.com>
2021-01-12 15:59:05 +0100

    test Scroll Pane

--- src/controller/ArticlePriceList.java
+++ src/controller/ArticlePriceList.java
@@ -6,6 +6,9 @@ import java.awt.Dimension;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 public class ArticlePriceList extends JPanel {
 
@@ -16,24 +19,50 @@ public class ArticlePriceList extends JPanel {
 
 	public ArticlePriceList() {
 		
-		
 		// right of the screen, price's and supplier's informations
 		
-		var priceList = new JPanel();
-		priceList.setPreferredSize(new Dimension(500, 0));
-		priceList.setLayout(new BoxLayout(priceList, BoxLayout.Y_AXIS));
+		this.setPreferredSize(new Dimension(500, 0));
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		var titlePriceList = new JLabel("Liste de prix");
-		priceList.add(titlePriceList);
+		this.add(titlePriceList);
 		
-		this.add(priceList);
 		
+		
+		
+		  String[] columnNames = {"First Name",
+                  "Last Name",
+                  "Sport",
+                  "# of Years",
+                  "Vegetarian"};
+		
+		  
+		  Object[][] data = {
+				    {"Kathy", "Smith",
+				     "Snowboarding", new Integer(5), new Boolean(false)},
+				    {"John", "Doe",
+				     "Rowing", new Integer(3), new Boolean(true)},
+				    {"Sue", "Black",
+				     "Knitting", new Integer(2), new Boolean(false)},
+				    {"Jane", "White",
+				     "Speed reading", new Integer(20), new Boolean(true)},
+				    {"Joe", "Brown",
+				     "Pool", new Integer(10), new Boolean(false)}
+				};
+		
+		  
+		  var listPrice = new JTable(data,columnNames);
+		  
+		  listPrice.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		  listPrice.setFillsViewportHeight(true);
+		  
+		// entre () on met ce qui doit scroller
+		// var scrollPriceList = new JScrollPane();
+	
+		  
+		  
+		this.add(listPrice);
+				
 	}
-	
-	
-
-	
-	
-	
 	
 }
