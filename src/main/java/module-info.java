dd3f2e32a2854967f9ed642cf1f76b05b2bc09e1
erikshea <71901477+erikshea@users.noreply.github.com>
2021-01-20 21:36:08 +0100

    Delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.nesti.stock_manager {
-    requires java.desktop;
-    requires mysql.connector.java;
-    requires hibernate.entitymanager;
-    exports com.nesti.stock_manager.application;
-}
